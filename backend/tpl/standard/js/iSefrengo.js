ddb2d5c404fa575b9924bec6131022f319db93c0
Andreas Fritsch <pawelf@web.de>
2010-11-28 17:28:50 +0100

    Save the height of .sftabs in the h Variable

--- backend/tpl/standard/js/iSefrengo.js
+++ backend/tpl/standard/js/iSefrengo.js
@@ -1,6 +1,8 @@
 $(document).ready(function(autoload) {
-$(".sftabs ul.menu a").click(function() {
+var h=$('.sftabs').height();
+$('.sftabs ul.menu a').click(function() {
 var a=$(this).attr("href");
-$(a).parent().parent().css( "height" , $(a).children().filter('div').height()*1.1);
+$('.sftabs').height(h);
+$(a).parent().parent().height($(a).children().filter('div').height()*1.1);
 	});// end click
 });//end function autoload
