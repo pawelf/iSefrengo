99f7fbdc5366c35ab92fee8d71f24d6d7b5d32f3
Andreas Fritsch <pawelf@web.de>
2010-11-28 15:20:44 +0100

    Styles now change one of .sftabs

--- backend/tpl/standard/js/iSefrengo.js
+++ backend/tpl/standard/js/iSefrengo.js
@@ -1,6 +1,6 @@
 $(document).ready(function(autoload) {
 $(".sftabs ul.menu a").click(function() {
 var a=$(this).attr("href");
-$(".sftabs").css( "height" , $(a).children().filter('div').height()*1.15);
+$(a).parent().parent().css( "height" , $(a).children().filter('div').height()*1.1);
 	});// end click
 });//end function autoload
