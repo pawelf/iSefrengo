001d8f7b3476dea59b874ea55788a24ca2919739
Andreas Fritsch <pawelf@web.de>
2011-02-25 16:00:37 +0100

    + Variables

--- backend/tpl/standard/lang/de/lang_con_configcat.php
+++ backend/tpl/standard/lang/de/lang_con_configcat.php
@@ -1,7 +1,32 @@
-<?php
-if(! defined('CMS_CONFIGFILE_INCLUDED')){
-	die('NO CONFIGFILE FOUND');
-}
+<?PHP
+/**
+  *
+  * Copyright (c) 2005 - 2007 sefrengo.org <info@sefrengo.org>
+  * Copyright (c) 2010 - 2011 iSefrengo
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License
+  *
+  * This program is subject to the GPL license, that is bundled with
+  * this package in the file LICENSE.TXT.
+  * If you did not receive a copy of the GNU General Public License
+  * along with this program write to the Free Software Foundation, Inc.,
+  * 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  *
+  * @author
+  */
+if(!defined('CMS_CONFIGFILE_INCLUDED')){die('NO CONFIGFILE FOUND');}
 
-	include_once($cfg_cms['cms_path'] .'tpl/standard/lang/'.$cfg_cms['backend_lang'].'/lang_con.php');
+  include_once($cfg_cms['cms_path'] .'tpl/standard/lang/'.$cfg_cms['backend_lang'].'/lang_con.php');
+
+$cms_lang['con_catconfig']				    = 'Ordnerangaben';
+$cms_lang['con_err_formular']					= 'Bitte pr&uuml;fen Sie Ihre Formulareingaben';
+$cms_lang['con_templateconfig']				= 'Templateangaben';
+
 ?>
